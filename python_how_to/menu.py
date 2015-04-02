a40844ee75a1743794a1f9c087eb0ca44812ba84
Dr Ric Colasanti <ric.colasanti@gmail.com>
2015-04-02 17:32:10 +0100

    menu and file open dialog example

---
+++ python_how_to/menu.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  untitled.py
+#  
+#  Copyright 2015  <ric.colasanti@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+# from http://www.python-course.eu/tkinter_menus.php
+#  
+
+
+
+from Tkinter import *
+from tkFileDialog   import askopenfilename
+
+def NewFile():
+    print "New File!"
+def OpenFile():
+    name = askopenfilename()
+    print name
+def About():
+    print "This is a simple example of a menu"
+    
+
+def main():
+    root = Tk()
+    menu = Menu(root)
+    root.config(menu=menu)
+    filemenu = Menu(menu)
+    menu.add_cascade(label="File", menu=filemenu)
+    filemenu.add_command(label="New", command=NewFile)
+    filemenu.add_command(label="Open...", command=OpenFile)
+    filemenu.add_separator()
+    filemenu.add_command(label="Exit", command=root.quit)
+
+    helpmenu = Menu(menu)
+    menu.add_cascade(label="Help", menu=helpmenu)
+    helpmenu.add_command(label="About...", command=About)
+    mainloop()
+    
+    return 0
+
+if __name__ == '__main__':
+    main()
+
